a353c9e60739d5e95b4fdd85b52c1e0116533269
James Sutherland <jrsutherland78@googlemail.com>
2018-08-02 08:56:22 +0100

    add number return in fizzbuzz test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,20 @@ describe 'fizzbuzz' do
   it 'returns fizz when passed 3' do
     expect(fizzbuzz(3)).to eq ('fizz')
   end
+
+  it 'returns buzz when passed 5' do
+    expect(fizzbuzz(5)).to eq ('buzz')
+  end
+
+  it 'returns fizzbuzz when passed 15' do
+    expect(fizzbuzz(15)).to eq ('fizzbuzz')
+  end
+
+  it 'returns 13 when passed 13' do
+    expect(fizzbuzz(13)).to eq (13)
+  end
+
+  it 'returns fizzbuzz when passed 30' do
+    expect(fizzbuzz(30)).to eq ('fizzbuzz')
+  end
 end
